20bcdadb81574632da975afd51ee3e12465840ef
Administrator <admin@example.com>
2015-06-15 17:01:36 +0700

    add run_dns

--- dns-server/run_dns.sh
+++ dns-server/run_dns.sh
@@ -1,5 +1,6 @@
 namenode_id=$(docker ps | grep namenode | awk '{print $1}')
 ip_namenode=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $namenode_id)
-echo 'address=/namenode/$i' >> /opt/dnsmasq.conf
+echo address=/namenode/$ip_namenode >> /opt/dnsmasq.conf
 flannel_ip=$(ip -o -4 addr list flannel0 | awk '{print $4}' | cut -d/ -f1)
-docker run -d -ti -p $flannel_ip:53:53/udp -v /opt/dnsmasq.conf:/etc/dnsmasq.conf dns-server
+docker run -ti -d -p $flannel_ip:53:53/udp -v /opt/dnsmasq.conf:/etc/dnsmasq.conf dns-server /usr/sbin/dnsmasq -d
+echo "dns-server ip" $flannel_ip
