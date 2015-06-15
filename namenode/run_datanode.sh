5fcbb4206ce8d757297be735dd35da75091bc52d
Administrator <admin@example.com>
2015-06-15 17:21:41 +0700

    edit README

--- namenode/run_datanode.sh
+++ namenode/run_datanode.sh
@@ -1,2 +1,2 @@
 docker build -t datanode .
-docker run -d -t -i --datanode --dns $1 datanode /etc/bootstrap.sh -bash -datanode
+docker run -d -t -i --datanode --dns=$1 datanode /etc/bootstrap.sh -bash -datanode
